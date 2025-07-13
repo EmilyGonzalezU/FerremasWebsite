7f6c314495cb0c95140a3a1308858cfacc83676a
Emily Gonzalez <em.gonzalezu@duocuc.cl>
2025-07-13 15:22:55 -0400

    -

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('registro/', views.registro_usuario, name='registro'),
     path('inicio sesion/', views.login_view, name='inicio_sesion'),
     path('cerrar_sesion/', views.cerrar_sesion, name='cerrar_sesion'),
-    path('', views.inicio, name='redireccion'),
+    path('', views.inicio_sesion, name='redireccion'),
     path('tipo_usuario/', views.tipo_usuario, name='tipo_usuario'),
 
 
